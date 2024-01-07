b30514da46e8a53b702e4c82f29ee522233f499a
AKot1988 <kulibabenko@ukr.net>
2024-01-07 17:08:21 +0200

    add firebase environment variable

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,7 @@ import { GoogleAuthProvider } from 'firebase/auth';
 
 // !! TODO: use environmental variables for the config, NOT hardcoded values
 const firebaseConfig = {
-  apiKey: 'AIzaSyAWlFZ7XmJJTWSy5ZhOyT1BFjDFK1cFOFw',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: 'ironplaygrounds.firebaseapp.com',
   projectId: 'ironplaygrounds',
   storageBucket: 'ironplaygrounds.appspot.com',
@@ -22,3 +22,5 @@ export const storageRef = ref(storage);
 export const playgroundCollectionRef = collection(db, 'playgrounds');
 export const favoritesCollectionRef = collection(db, 'favorites');
 export const googleAuthProvider = new GoogleAuthProvider();
+
+
